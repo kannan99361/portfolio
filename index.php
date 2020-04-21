12269c352c9245e8848042d1767c1578df53ca47
kannan99361 <57428064+kannan99361@users.noreply.github.com>
2020-04-20 21:43:49 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Engineering Portfolio</title>
+    <link href="bootstrap-4.1.2-dist/css/bootstrap.css" type='text/css' rel="stylesheet">
+  </head>
+
+  <body>
+
+
+  <div class="container">
+    <nav class="nav nav-pills nav-fill">
+      <a class="nav-item nav link active" href="index.html">Home</a>
+      <a class="nav-item nav link active" href="Kannan_AboutMe.html">About me</a>
+      <a class="nav-item nav link active" href="Kannan_ContactMe.html">Contact Me</a>
+      <a class="nav-item nav link active" href="Kannan">Resume</a>
+
+  <div class="jumbotron">
+    <div>
+      <h1>Darshani's Engineering Portfolio</h1>
+      <p>Hello! Welcome to my website!</p>
+      <p><a href="Brown_AboutMe.html">Continue Reading...</a></p>
+    </div>
+  </div>
+</div>
+
+
+
